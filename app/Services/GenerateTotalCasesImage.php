7c114b52da3100cf9e589d42c760c89ffa7696f1
Robin <hej@robin.se>
2020-04-18 00:36:54 +0800

    Move fonts and templates to resources

--- app/Services/GenerateTotalCasesImage.php
+++ app/Services/GenerateTotalCasesImage.php
@@ -10,21 +10,21 @@ class GenerateTotalCasesImage
     {
         $time = date('H:i   j M Y');
 
-        return Image::make(storage_path('image-templates/total-cases.png'))
+        return Image::make(resource_path('image-templates/total-cases.png'))
             ->text($confirmed, 1050, 325, function ($font) {
-                $font->file(storage_path('fonts/Avenir-Black.ttf'));
+                $font->file(resource_path('fonts/Avenir-Black.ttf'));
                 $font->align('right');
                 $font->size(130);
                 $font->color('#290303');
             })
             ->text($deaths, 1050, 555, function ($font) {
-                $font->file(storage_path('fonts/Avenir-Black.ttf'));
+                $font->file(resource_path('fonts/Avenir-Black.ttf'));
                 $font->align('right');
                 $font->size(130);
                 $font->color('#290303');
             })
             ->text(mb_strtoupper($time), 280, 101, function ($font) {
-                $font->file(storage_path('fonts/Avenir-Black.ttf'));
+                $font->file(resource_path('fonts/Avenir-Black.ttf'));
                 $font->size(72);
                 $font->color('#fff');
             });
