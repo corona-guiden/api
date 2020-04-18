e17b8759794da00a9fccf12ff519c4c7337c3353
Robin <hej@robin.se>
2020-04-18 21:17:09 +0800

    Cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,5 @@
 <?php
 
-use App\Imports\CoronaStatsImport;
-use App\RegionStat;
-use App\Services\GenerateTotalCasesImage;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
-use Maatwebsite\Excel\Facades\Excel;
-use Zttp\Zttp;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::view('/', 'home');
