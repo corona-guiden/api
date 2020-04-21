0317c38292147e500dc66b5c535a61ee99b707bc
Robin <hej@robin.se>
2020-04-21 22:24:23 +0800

    Schedule ScrapeWebsiteMonitors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Imports\CoronaStatsImport;
 use App\Jobs\ScrapeSuggestions;
+use App\Jobs\ScrapeWebsiteMonitors;
 use App\RegionStat;
 use App\Services\GenerateTotalCasesImage;
 use Illuminate\Console\Scheduling\Schedule;
@@ -33,7 +34,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(ScrapeSuggestions::class)->everyFiveMinutes();
-
+        $schedule->job(ScrapeWebsiteMonitors::class)->hourly();
         $schedule->call(function() {
             $this->call('import:stats');
             $this->call('generate-image:total-cases');
