3e481b65df79ea43b8b91abae72591f38d380a36
Robin <hej@robin.se>
2020-04-21 22:14:55 +0800

    Removed tests

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Symfony\Component\DomCrawler\Crawler;
 
 Route::view('/', 'home');
 
-Route::get('test', function() {
-    $url = 'https://www.1177.se/stockholm/sjukdomar--besvar/lungor-och-luftvagar/inflammation-och-infektion-ilungor-och-luftror/covid-19-coronavirus/';
-
-    $job = new \App\Jobs\ScrapeWebsiteMonitors($url, '.c-articlefooter__wrapper .c-articlefooter__value', '#content');
-
-    $job->handle();
-});
+//Route::get('test', function() {
+//    $url = 'https://www.1177.se/stockholm/sjukdomar--besvar/lungor-och-luftvagar/inflammation-och-infektion-ilungor-och-luftror/covid-19-coronavirus/';
+//
+//    $job = new \App\Jobs\ScrapeWebsiteMonitors($url, '.c-articlefooter__wrapper .c-articlefooter__value', '#content');
+//
+//    $job->handle();
+//});
